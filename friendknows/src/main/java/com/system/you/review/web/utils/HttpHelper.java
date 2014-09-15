bbd7413440616cb26c539d54d9380a3649d8be39
tarunagrawal <mail2tarunagrawal@gmail.com>
2014-09-15 21:53:18 +0530

    Changes for mySql

--- friendknows/src/main/java/com/system/you/review/web/utils/HttpHelper.java
+++ friendknows/src/main/java/com/system/you/review/web/utils/HttpHelper.java
@@ -6,25 +6,25 @@ import org.apache.commons.lang.StringUtils;
 
 public class HttpHelper {
 
-	public static String URLSeperartor = "/";
-
 	public static String getHomeURL(HttpServletRequest aRequest) {
 		String requestURL = aRequest.getRequestURL().toString();
 		String context = aRequest.getContextPath();
 		if (StringUtils.isNotBlank(context)) {
-			//This code deals with local development where context path is used.
+			// This code deals with local development where context path is
+			// used.
 			return getHomeUrlWithContext(requestURL, context);
 		}
-		//Now, should reply on request's server 
-		System.out.println(aRequest.getServerName());
-		return "http://" +aRequest.getServerName();
+		// Now, should rely on request's server
+		return HTTP_PROTOCOL + aRequest.getServerName();
 	}
 
 	private static String getHomeUrlWithContext(String requestURL,
 			String context) {
-		String homeURL = requestURL.substring(0,
-				requestURL.indexOf(context));
-		homeURL = StringUtils.join(new Object[] { homeURL, context });
+		String homeURL = requestURL.substring(0, requestURL.indexOf(context));
+		homeURL = StringUtils.join(new String[] { homeURL, context });
 		return homeURL;
 	}
+
+	private static final String HTTP_PROTOCOL = "http://";
+	public static String URLSeperartor = "/";
 }
