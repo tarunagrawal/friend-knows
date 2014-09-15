1c7b165f3b9193bb43af14b1da61f7beeda9c396
tarunagrawal <mail2tarunagrawal@gmail.com>
2014-09-15 18:37:00 +0530

    working for relative URLs and facebook changes.

--- friendknows/src/main/java/com/system/you/review/web/utils/HttpHelper.java
+++ friendknows/src/main/java/com/system/you/review/web/utils/HttpHelper.java
@@ -6,14 +6,25 @@ import org.apache.commons.lang.StringUtils;
 
 public class HttpHelper {
 
-	public static String URLSeperartor = "/"; 
-	
-	public static String getHomeURL(HttpServletRequest aRequest){
+	public static String URLSeperartor = "/";
+
+	public static String getHomeURL(HttpServletRequest aRequest) {
 		String requestURL = aRequest.getRequestURL().toString();
 		String context = aRequest.getContextPath();
-		String homeURL = requestURL.substring(0,requestURL.indexOf(context));
-		System.out.println(homeURL);
-		homeURL = StringUtils.join(new Object[]{homeURL,context});
+		if (StringUtils.isNotBlank(context)) {
+			//This code deals with local development where context path is used.
+			return getHomeUrlWithContext(requestURL, context);
+		}
+		//Now, should reply on request's server 
+		System.out.println(aRequest.getServerName());
+		return "http://" +aRequest.getServerName();
+	}
+
+	private static String getHomeUrlWithContext(String requestURL,
+			String context) {
+		String homeURL = requestURL.substring(0,
+				requestURL.indexOf(context));
+		homeURL = StringUtils.join(new Object[] { homeURL, context });
 		return homeURL;
 	}
 }
